9fd9904bdf28d56933e46df3b5452e99b2ae6f65
drakenevadie19 <drakennievadje19@gmail.com>
2025-04-23 23:20:08 -0500

    Initialize project

---
+++ sudokugame/src/main/java/sudoku/sudokugame/SudokuGameBackEndApplication.java
@@ -0,0 +1,13 @@
+package sudoku.sudokugame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokuGameBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudokuGameBackEndApplication.class, args);
+	}
+
+}
---
+++ sudokugame/src/test/java/sudoku/sudokugame/SudokuGameBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package sudoku.sudokugame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuGameBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
